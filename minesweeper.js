c8e972c63173be378d908148fa8360ce6d37cec4
Anna P <podhajskaa@gmail.com>
2017-06-05 15:40:17 +1200

    finished 6.2

--- minesweeper.js
+++ minesweeper.js
@@ -16,6 +16,17 @@ var board = {
 }
 
 function startGame () {
+
+//ANIA:
+
+  for (var i = 0; i < board.cells.length; i++) {
+
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+
+
+//:ANIA
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -40,4 +51,12 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+  for (var i = 0; i < surrounding.length; i++) {
+    if (surrounding[i].isMine) {
+      count += 1;
+    }
+  }
+  return count;
 }
